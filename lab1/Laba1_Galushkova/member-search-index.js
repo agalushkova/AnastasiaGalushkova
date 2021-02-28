d967a59b5e417fe3aa6ce970de08fc1746dee95b
agalushkova <72885283+agalushkova@users.noreply.github.com>
2021-02-28 17:08:39 +0200

    Delete lab1 directory

--- lab1/Laba1_Galushkova/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Initials","l":"Initials()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Initials","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
